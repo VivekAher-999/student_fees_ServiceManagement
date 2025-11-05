2932f59ccfc5893da1880515084d8d4cfded6031
VivekAher-999 <aherv99@gmail.com>
2025-11-05 12:20:59 +0530

    Some Changes Added

--- student-service/src/main/java/com/example/controller/StudentController.java
+++ student-service/src/main/java/com/example/controller/StudentController.java
@@ -19,6 +19,7 @@ import com.example.service.StudentService;
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+	
 	private final StudentRepository repo;
 
 	@Autowired
